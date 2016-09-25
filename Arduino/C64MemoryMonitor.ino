695f80330cbe8106210450209fe339861242ede4
Leif Bloomquist <github@leifbloomquist.net>
2016-09-25 01:53:06 -0400

    Added in NeoPixels

--- Arduino/C64MemoryMonitor.ino
+++ Arduino/C64MemoryMonitor.ino
@@ -1,8 +1,11 @@
 // ----------------------------------------------------------
 // C64 Visual Memory Monitor 
 
+#include <Adafruit_NeoPixel.h>
 #include <digitalWriteFast.h>
 
+#define LEDS     64
+#define PIN_NEO  A1 
 #define PIN_PHI2 2   // INT0
 #define PIN_RW   A0
 
@@ -16,12 +19,16 @@
 #define PIN_A11  9     // PB1
 #define PIN_A10  8     // PB0
 
+volatile unsigned int rw = 0;
 volatile unsigned long phi2_cnt = 0;
 volatile uint8_t address = 0;
 
-volatile int rw = 0;
-volatile unsigned long reads[64] = { 0 };
-volatile unsigned long writes[64] = { 0 };
+volatile unsigned long reads[LEDS] = { 0 };
+volatile unsigned long writes[LEDS] = { 0 };
+
+int counter = 0;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN_NEO, NEO_GRB + NEO_KHZ800);
 
 void setup(void)
 {
@@ -32,14 +39,51 @@ void setup(void)
   attachInterrupt(digitalPinToInterrupt(PIN_PHI2), phi2_isr, RISING);
 
   DDRB = B00000000;   //  All inputs for address lines
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
+
+int red = 0;
+int green = 0;
+int i = 0;
+
 void loop()
+{
+  for (i = 0; i < LEDS; i++)
+  {    
+    red =  (writes[i] / phi2_cnt) * 255;
+    green = (reads[i] / phi2_cnt) * 255;   
+    strip.setPixelColor(i, red, green, 0);   
+
+    writes[i] = 0;
+    reads[i] = 0;    
+  }
+  
+ 
+  phi2_cnt = 0;
+
+  counter++;
+  if (counter >= LEDS)
+  {
+    counter = 0;
+  }
+
+  strip.setPixelColor(counter, 0, 0, 255);
+  strip.show();
+
+  //Serial.println(counter);
+  //delay(10); 
+}
+
+
+void loop_debug()
 {
   Serial.println(phi2_cnt);
   phi2_cnt = 0;
 
-  for (int i = 0; i < 64; i++)
+  for (int i = 0; i < LEDS; i++)
   {
     Serial.print(reads[i]);
     reads[i] = 0;
@@ -48,7 +92,7 @@ void loop()
   }
   Serial.println();
 
-  for (int i = 0; i < 64; i++)
+  for (int i = 0; i < LEDS; i++)
   {
     Serial.print(writes[i]);
     writes[i] = 0;
