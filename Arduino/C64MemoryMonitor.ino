66adc80acbf6f93449394eb2ac0fc36db051b466
Leif Bloomquist <github@leifbloomquist.net>
2016-09-27 00:25:55 -0400

    Simplified ISR&Logic

--- Arduino/C64MemoryMonitor.ino
+++ Arduino/C64MemoryMonitor.ino
@@ -21,12 +21,12 @@
 #define PIN_A11  9     // PB1
 #define PIN_A10  8     // PB0
 
-volatile unsigned int rw = 0;
-volatile unsigned long phi2_cnt = 0;
+volatile byte rw = 0;
+//volatile unsigned long phi2_cnt = 0;
 volatile uint8_t address = 0;
 
-volatile unsigned long reads[LEDS] = { 0 };
-volatile unsigned long writes[LEDS] = { 0 };
+volatile byte reads[LEDS] = { 0 };
+volatile byte writes[LEDS] = { 0 };
 
 int counter = 0;
 
@@ -34,13 +34,16 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN_NEO, NEO_GRB + NEO_KHZ800)
 
 void setup(void)
 {
-  Serial.begin(115200);
+  //Serial.begin(115200);
 
   pinModeFast(PIN_RW, INPUT);
   pinModeFast(PIN_PHI2, INPUT);
   attachInterrupt(digitalPinToInterrupt(PIN_PHI2), phi2_isr, RISING);
 
   DDRB = B00000000;   //  All inputs for address lines
+
+  // Enable the pullups for our six inputs
+  PORTB = PORTB | B00111111;
 
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
@@ -51,57 +54,22 @@ int red = 0;
 int green = 0;
 int i = 0;
 
-int w = 0;
-int r = 0;
-
 void loop()
 {
   for (i = 0; i < LEDS; i++)
   { 
-    w = writes[i];
-    r = reads[i];
+    strip.setPixelColor(i, writes[i], reads[i], 0);
 
-    if (w > 0)    
-    {
-       red = map(w, 0, phi2_cnt, 100, 255);
-    }
-    else
-    {
-      red = 0;
-    }
-    
-    if (r > 0)
-    {
-      green = map(r, 0, phi2_cnt, 100, 255);
-    }
-    else
-    {
-      green = 0;
-    }
-
-    strip.setPixelColor(i, red, green, 0);   
-
-    writes[i] = 0;
-    reads[i] = 0;    
+    writes[i] = writes[i] >> 1;
+    reads[i] = reads[i] >> 1;
   }
 
-  //Serial.println(phi2_cnt);
-  phi2_cnt = 0; 
-
-  counter++;
-  if (counter >= LEDS)
-  {
-    counter = 0;
-  }
-
-  strip.setPixelColor(counter, 0, 0, 255);
+  counter = !counter;
+  strip.setPixelColor(59, 0, 0, counter*255);
   strip.show();
-
-  //Serial.println(counter);
- // delay(10); 
 }
 
-
+/*
 void loop_debug()
 {
   Serial.println(phi2_cnt);
@@ -128,24 +96,25 @@ void loop_debug()
 
   delay(1000);
 }
+*/
 
 // ISR
 
 void phi2_isr()
 { 
   rw = digitalReadFast(PIN_RW);
-  address = ~PINB & B00111111;  // Invert (active low) and mask out the crystal inputs on upper two bits
+  address = PINB & B00111111;  // Invert (active low) and mask out the crystal inputs on upper two bits
 
   if (rw)
   {
-    reads[address]++;   // High is a read
+    reads[address]=255;   // High is a read
   }
   else
   {
-    writes[address]++;  // Low is a write
+    writes[address]=255;  // Low is a write
   }
 
-  phi2_cnt++;
+  //phi2_cnt++;
 }
 
 // #EOF
